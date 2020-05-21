910d9dee9761a8afd90500a54cc01e934545bb01
dariavarentsova <63418397+dariavarentsova@users.noreply.github.com>
2020-05-21 20:26:36 +0300

    Create Alg.cpp

---
+++ src/Alg.cpp
@@ -0,0 +1,64 @@
+int countPairs1(int* arr, int len, int value)
+{
+int count = 0;
+for (int i = 0; i < len-1; i++)
+{
+for (int j = i + 1; j < len; j++)
+{
+if (arr[i] + arr[j] == value)
+count ++;
+}
+}
+return count;
+}
+
+int countPairs2(int* arr, int len, int value)
+{
+int count = 0;
+for (int i = 0; i < len - 1; i++)
+{
+for (int j = len - 1; j > i; j--)
+{
+if (arr[i] + arr[j] == value)
+{
+count++;
+}
+}
+}
+return count;
+}
+
+int countPairs3(int* arr, int len, int value)
+{int count = 0;
+for (int i = 0; i < len - 1; i++)
+{
+int left = i, right = len;
+
+while (left < right-1)
+{
+int mid = (left + right) / 2;
+if (arr[i] + arr[mid] == value)
+{
+count++;
+int j = mid+1;
+while (arr[i] + arr[j] == value && j <right)
+{
+count++;
+j++;
+}
+j = mid - 1;
+while (arr[i] + arr[j] == value && j > left)
+{
+count++;
+j--;
+}
+break;
+}
+if (arr[i] + arr[mid] > value)
+right = mid;
+else
+left = mid;
+}
+}
+return count;
+}
